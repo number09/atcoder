d9a034541e9fe16669ed5267f986eb207e287d5d
number09 <cielo.abierto09@gmail.com>
2018-05-13 12:25:45 +0900

    abc049-c

---
+++ abc049-c.py
@@ -0,0 +1,28 @@
+int_s = input()
+str_sr = int_s[::-1]
+
+
+str_a = ['dream', 'dreamer', 'erase', 'eraser']
+
+str_r = list(map(lambda x: x[::-1], str_a))
+
+result = True
+
+while len(str_sr):
+    currnt_flg = False
+    for r in str_r:
+        print('r:' + r)
+        if len(str_sr) >= len(r) and str_sr[:len(r)] == r:
+            print('ok')
+            str_sr = str_sr[len(r):]
+            print(str_sr)
+            current_flg = True
+            break
+    else:
+        result = False
+        break
+
+if result:
+    print('YES')
+else:
+    print('No')
