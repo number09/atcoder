584d95c5275c3c56d1d5fed45f48e56a5ee733b3
number09 <cielo.abierto09@gmail.com>
2018-05-15 07:17:04 +0900

    abc097-a

---
+++ abc097-a.py
@@ -0,0 +1,19 @@
+int_a, int_b, int_c, int_d = map(int, input().split())
+
+result = False
+
+if abs(int_a - int_c) <= int_d:
+    result = True
+elif abs(int_a - int_b) <= int_d and abs(int_b - int_c) <= int_d:
+    result = True
+
+if result:
+    print('YES')
+else:
+    print('No')
+
+
+
+
+
+
