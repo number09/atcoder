8a73507c5edfa073510a78f7d38110c3b87eb605
Koichi Ogura <cielo.abierto09@gmail.com>
2018-06-10 10:36:43 +0900

    abc098-c

---
+++ abc098-c.py
@@ -0,0 +1,19 @@
+int_n = int(input())
+str_s = input()
+
+changes = 999999
+
+for i in range(int_n):
+    member_left = str_s[:i]
+    member_right = str_s[i + 1:]
+    # print(i)
+    # print("left:" + member_left, "right:" + member_right)
+
+    this_change = (member_left.count("W") + member_right.count("E"))
+
+    changes = this_change if (changes >= this_change) else changes
+
+print(changes)
+
+
+
