5401b037ac8510183d210297acea079464c1c0f4
Koichi Ogura <cielo.abierto09@gmail.com>
2018-07-12 06:36:40 +0900

    abc073-c

---
+++ abc073-c.py
@@ -0,0 +1,11 @@
+int_n = int(input())
+
+ar_int_a = []
+for i in range(int_n):
+    ar_int_a.append(int(input()))
+
+numbers = set(ar_int_a)
+
+written = [i for i in numbers if ar_int_a.count(i) % 2 == 1]
+
+print(len(written))
