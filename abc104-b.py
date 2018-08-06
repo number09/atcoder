df825d4a0587d8774e233ed00f211f1450ddbf61
Koichi Ogura <cielo.abierto09@gmail.com>
2018-08-07 06:01:16 +0900

    abc104-b

---
+++ abc104-b.py
@@ -0,0 +1,24 @@
+import re
+
+str_s = input()
+
+if str_s[0] != 'A':
+    print("WA")
+    exit(0)
+
+if str_s[2:-1].count("C") != 1:
+    print("WA")
+    exit(0)
+
+idx_c = str_s.find("C")
+
+w_str = str_s[1:idx_c] + str_s[idx_c +1:]
+if w_str.islower():
+    print("AC")
+else:
+    print("WA")
+
+
+
+
+
