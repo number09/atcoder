3c9b86d06a7734b090ca7e376e33e43559cfcfae
Koichi Ogura <cielo.abierto09@gmail.com>
2018-09-15 11:08:47 +0900

    abc054-b

---
+++ abc054-b.py
@@ -0,0 +1,21 @@
+int_n, int_m = map(int, input().split())
+
+li_a = list()
+li_b = list()
+
+for i in range(int_n):
+    li_a.append(input())
+
+for i in range(int_m):
+    li_b.append(input())
+
+
+for x in range(int_n - int_m):
+    for y in range(int_n - int_m):
+        li_w = [a[0 + x: int_m + x] for a in li_a[0 + y: int_m + y]]
+
+        if "".join(li_w) == "".join(li_b):
+            print("Yes")
+            exit(0)
+else:
+    print("No")
