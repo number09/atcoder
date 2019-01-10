2742c346cc90581c3b500fe7afcb646af5a3fba0
ogura <cielo.abierto09@gmail.com>
2019-01-11 05:31:37 +0900

    abc058-b MOD

--- abc058-b.py
+++ abc058-b.py
@@ -1,4 +1,5 @@
+from itertools import zip_longest
 str_o = input()
 str_e = input()
 
-print("".join(["".join([o, e]) for o, e in zip(str_o, str_e)]))
+print("".join(["".join([o, e]) for o, e in zip_longest(str_o, str_e, fillvalue="")]))
