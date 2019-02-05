2b5bce40cb419679955ef153682f9976456488cf
Koichi Ogura <cielo.abierto09@gmail.com>
2019-02-06 00:32:21 +0900

    abc118-c(TLE)

--- abc018-c.py
+++ abc018-c.py
@@ -1,22 +1,44 @@
 r, c, k = map(int, input().split())
 
 li_s = list()
+for i in range(r):
+    li_s.append(list(input()))
 
 answer = 0
 
 
+li_check = list()
+for t_r in range(r):
+    li_check.append([""] * c)
+    for t_c in range(c):
+        if li_s[t_r][t_c] == 'x':
+            li_check[t_r][t_c] = '0/0'
+        else:
+            upcount = 0
+            for up in range(t_r, -1, -1):
+                if li_s[up][t_c] != 'x':
+                    upcount += 1
+                else:
+                    break
+            downcount = 0
+            for down in range(t_r, r):
+                if li_s[down][t_c] != 'x':
+                    downcount += 1
+                else:
+                    break
+
+            li_check[t_r][t_c] = str(upcount) + '/' + str(downcount)
+
+
+
 def check_zyoge(in_r, in_c, in_range):
 
-    for w in range(in_range):
-        if li_s[in_r - w][in_c] == 'x' or li_s[in_r + w][in_c] == 'x':
-            break
-    else:
+    upcount, downcount = map(int, li_check[in_r][in_c].split("/"))
+    if upcount >= in_range and downcount >= in_range:
         return True
+    else:
+        return False
 
-    return False
-
-for i in range(r):
-    li_s.append(list(input()))
 
 for tmp_r in range(r):
     for tmp_c in range(c):
