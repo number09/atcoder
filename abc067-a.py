82266e7668714d98aef56c5e3f7f316bdb58f966
Koichi Ogura <cielo.abierto09@gmail.com>
2019-02-09 08:19:18 +0900

    mod abc067-a

--- abc067-a.py
+++ abc067-a.py
@@ -1,6 +1,6 @@
 int_a, int_b = map(int, input().split())
 
-if (int_a + int_b) % 3 == 0:
+if (int_a + int_b) % 3 == 0 or int_a % 3 == 0 or int_b % 3 == 0:
     print("Possible")
 else:
     print("Impossible")
