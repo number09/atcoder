b09d4008a1d77a29667ec62cc5c76d07924b1f19
ogura <cielo.abierto09@gmail.com>
2019-02-14 12:56:51 +0900

    mod abc054-b

--- abc054-b.py
+++ abc054-b.py
@@ -10,10 +10,9 @@ for i in range(int_m):
     li_b.append(input())
 
 
-for x in range(int_n - int_m):
-    for y in range(int_n - int_m):
+for x in range(int_n - int_m + 1):
+    for y in range(int_n - int_m + 1):
         li_w = [a[0 + x: int_m + x] for a in li_a[0 + y: int_m + y]]
-
         if "".join(li_w) == "".join(li_b):
             print("Yes")
             exit(0)
