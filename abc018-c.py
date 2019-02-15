3c65c1a92b29ac8d60ce51fa23b75524b0c56b84
Koichi Ogura <cielo.abierto09@gmail.com>
2019-02-15 23:39:16 +0900

    abc018-c (AC PyPy3 not Python3)

--- abc018-c.py
+++ abc018-c.py
@@ -7,71 +7,55 @@ for i in range(r):
 answer = 0
 
 
-li_check = list()
+li_check_up = list()
 for t_r in range(r):
-    li_check.append([""] * c)
+    li_check_up.append([0] * c)
     for t_c in range(c):
         if li_s[t_r][t_c] == 'x':
-            li_check[t_r][t_c] = '0/0'
+            pass
         else:
-            upcount = 0
             # r = 0の場合は自分自身の値で判定する
             # r >=1の場合は、上段(r -1) の値 + 1
             if t_r == 0:
-                    upcount += 1
+                li_check_up[t_r][t_c] = 1
             else:
-                upcount = int(li_check[t_r - 1][t_c].split('/')[0]) + 1
+                li_check_up[t_r][t_c] = li_check_up[t_r - 1][t_c] + 1
 
-            # for up in range(t_r, -1, -1):
-            #     if li_s[up][t_c] != 'x':
-            #         upcount += 1
-            #     else:
-            #         break
-
-            downcount = 0
-            # r = 0または 一つ上段がxの場合、素直にループで判定
-            # 上記以外の場合は、上段(r -1) の値 - 1
-            if t_r == 0 or li_s[t_r - 1][t_c] == 'x':
-                for down in range(t_r, r):
-                    if li_s[down][t_c] != 'x':
-                        downcount += 1
-                    else:
-                        break
+li_check_down = [""] * r
+for t_r in reversed(range(r)):
+    li_check_down[t_r] = ([0] * c)
+    for t_c in range(c):
+        if li_s[t_r][t_c] == 'x':
+            pass
+        else:
+            # r = r -1 の場合は自分自身の値で判定する
+            # 上記以外の場合は、下段(r +1) の値 + 1
+            if t_r == (r - 1):
+                li_check_down[t_r][t_c] = 1
             else:
-                downcount = int(li_check[t_r - 1][t_c].split('/')[1]) - 1
+                li_check_down[t_r][t_c] = li_check_down[t_r + 1][t_c] + 1
 
-
-            # for down in range(t_r, r):
-            #     if li_s[down][t_c] != 'x':
-            #         downcount += 1
-            #     else:
-            #         break
-
-            li_check[t_r][t_c] = str(upcount) + '/' + str(downcount)
-
-# print(li_check)
 
 def check_zyoge(in_r, in_c, in_range):
 
-    upcount, downcount = map(int, li_check[in_r][in_c].split("/"))
+    upcount = li_check_up[in_r][in_c]
+    downcount = li_check_down[in_r][in_c]
     if upcount >= in_range and downcount >= in_range:
         return True
     else:
         return False
 
 
-# for tmp_r in range(r):
-#     for tmp_c in range(c):
-#         #tmp_r,tmp_cを中央座標とした場合、上下左右にkマス以上あるか確認
-#         if r - k >= tmp_r >= k - 1 and c - k >= tmp_c >= k - 1:
-#             if all(map(lambda x: check_zyoge(tmp_r, x, k - abs(tmp_c - x)), [yoko for yoko in range(tmp_c - k +1, tmp_c + k)])):
-#                 answer += 1
-#         else:
-#             pass
-
 for tmp_r in range(k - 1, r - k + 1):
     for tmp_c in range(k - 1, c - k + 1):
-        if all(map(lambda x: check_zyoge(tmp_r, x, k - abs(tmp_c - x)), [yoko for yoko in range(tmp_c - k +1, tmp_c + k)])):
+        # if all(map(lambda x: check_zyoge(tmp_r, x, k - abs(tmp_c - x)), (yoko for yoko in range(tmp_c - k + 1, tmp_c + k)))):
+        #    answer += 1
+        for yoko in range(tmp_c - k + 1, tmp_c + k):
+            if not check_zyoge(tmp_r, yoko, k - abs(tmp_c - yoko)):
+                break
+        else:
             answer += 1
+
+
 
 print(answer)
