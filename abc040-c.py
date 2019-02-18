29e494cbf6e78508458f8923d38e931f3afa4ed2
Koichi Ogura <cielo.abierto09@gmail.com>
2019-02-18 21:51:33 +0900

    abc040-c

--- abc040-c.py
+++ abc040-c.py
@@ -2,23 +2,20 @@ n = int(input())
 
 li_height = list(map(int, input().split()))
 
-li_cost = [99999] * n
+li_cost = [99999 ** 10] * n
 li_cost[0] = 0
 
 for from_index in range(n):
     to_1 = from_index + 1
     if to_1 <= (n - 1):
         cost = abs(li_height[from_index] - li_height[to_1]) + li_cost[from_index]
-        if li_cost[to_1] > cost:
-            li_cost[to_1] = cost
+        li_cost[to_1] = min(li_cost[to_1], cost)
 
     to_2 = from_index + 2
     if to_2 <= (n - 1):
         cost = abs(li_height[from_index] - li_height[to_2]) + li_cost[from_index]
-        if li_cost[to_2] > cost:
-            li_cost[to_2] = cost
+        li_cost[to_2] = min(li_cost[to_2], cost)
 
-print(li_cost)
 print(li_cost[-1])
 
 
