3b38d46caf95440b737f438d4f1d9c9deeaf7aaa
Koichi Ogura <cielo.abierto09@gmail.com>
2019-02-25 23:49:22 +0900

    abc072-c mod

--- abc072-c.py
+++ abc072-c.py
@@ -1,12 +1,18 @@
+from collections import defaultdict
+
 int_n = int(input())
 ar_int_a = list(map(int, input().split()))
+
+d_cnt = defaultdict(int)
+for w in ar_int_a:
+    d_cnt[w] += 1
+
 
 st_int_group = set(ar_int_a)
 max_count = 0
 
 for i in st_int_group:
-    current = ar_int_a.count(i) + ar_int_a.count(i - 1) + ar_int_a.count(i + 1)
-    max_count = current if max_count <= current else max_count
+    max_count = max(d_cnt[i] + d_cnt[i - 1] + d_cnt[i + 1], max_count)
 
 print(max_count)
 
