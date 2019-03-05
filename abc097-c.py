1479721e86249160a87c9742952573252f7632e6
Koichi Ogura <cielo.abierto09@gmail.com>
2019-03-05 23:31:35 +0900

    abc097-c mod

--- abc097-c.py
+++ abc097-c.py
@@ -1,12 +1,12 @@
 str_s = input()
 int_k = int(input())
 
-pattern = set()
-
-for f in range(len(str_s) + 1):
-    for t in range(f + 1, len(str_s) + 1):
+result = list()
+for f in range(len(str_s)):
+    pattern = set()
+    for t in range(f + 1, f + 1 + int_k):
         pattern.add(str_s[f:t])
+        result = sorted(set(result + list(pattern)))[:int_k]
 
-print(sorted(list(pattern))[int_k - 1])
-
+print(result[-1])
 
