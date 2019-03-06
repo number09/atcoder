3cb7afe6e2250da9cbfc3c23b191172c22d534bf
Koichi Ogura <cielo.abierto09@gmail.com>
2019-03-06 21:40:26 +0900

    abc061-c mod

--- abc061-c.py
+++ abc061-c.py
@@ -1,10 +1,18 @@
 int_n, int_k = map(int, input().split())
 
-ar_int = list()
+li_wk = list()
 
 for i in range(int_n):
-    int_a, int_b = map(int, input().split())
-    for x in range(int_b):
-        ar_int.append(int_a)
+    li_wk.append(list(map(int, input().split())))
 
-print(sorted(ar_int)[int_k - 1])
+li_wk.sort(key=lambda x: x[0])
+
+counter = 1
+for l in li_wk:
+    if counter <= int_k <= (counter + l[1] - 1):
+        print(l[0])
+    counter += l[1]
+
+
+
+
