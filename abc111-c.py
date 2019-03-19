d8dbbeb150ff47342e617cb32a8a24b7dd895b29
Koichi Ogura <cielo.abierto09@gmail.com>
2019-03-19 22:59:58 +0900

    abc111-c mod

--- abc111-c.py
+++ abc111-c.py
@@ -1,47 +1,34 @@
+from collections import defaultdict
+
 n = int(input())
 li_input = list(map(int, input().split()))
 
 li_ki = li_input[0::2]
+d_ki = defaultdict(int)
+for ki in li_ki:
+    d_ki[ki] += 1
+
+
 li_gu = li_input[1::2]
+d_gu = defaultdict(int)
+for gu in li_gu:
+    d_gu[gu] += 1
 
+ki_sort = sorted([(k, v) for k,v in d_ki.items()], key=lambda x : x[1], reverse=True)
+gu_sort = sorted([(k, v) for k,v in d_gu.items()], key=lambda x : x[1], reverse=True)
 
-li_ki_counter = sorted([(i, li_ki.count(i)) for i in set(li_ki)], key=lambda x: x[1])
-li_gu_counter = sorted([(i, li_gu.count(i)) for i in set(li_gu)], key=lambda x: x[1])
+ki_cnt = 0
+gu_cnt = 0
 
-
-ki_1st = li_ki_counter[0][0]
-gu_1st = li_gu_counter[0][0]
-
-if ki_1st == gu_1st:
-    # 最頻値が同じ値の場合、2番目に多い要素を使う
-
-    if len(li_ki_counter) == 1:
-        ki_2nd = -1
-    else:
-        ki_2nd = li_ki_counter[1][0]
-
-    if len(li_gu_counter) == 1:
-        gu_2nd = -1
-    else:
-        gu_2nd = li_gu_counter[1][0]
-
-    ptn1 = len(list(filter(lambda x: x != ki_1st, li_ki))) + len(list(filter(lambda x: x != gu_2nd, li_gu)))
-    ptn2 = len(list(filter(lambda x: x != ki_2nd, li_ki))) + len(list(filter(lambda x: x != gu_1st, li_gu)))
-
-    if ptn1 >= ptn2:
-        print(ptn2)
-    else:
-        print(ptn1)
-
+if ki_sort[0][0] != gu_sort[0][0]:
+    print(n - ki_sort[0][1] - gu_sort[0][1])
 else:
-    # 最頻値が異なる場合
-    print(
-        len(list(filter(lambda x: x != ki_1st, li_ki))) +
-        len(list(filter(lambda x: x != gu_1st, li_gu)))
-    )
+    ki_1 = ki_sort[0][1]
+    ki_2 = ki_sort[1][1] if len(ki_sort) > 1 else 0
 
+    gu_1 = gu_sort[0][1]
+    gu_2 = gu_sort[1][1] if len(gu_sort) > 1 else 0
 
-
-
+    print(min(n - ki_1 - gu_2, n - ki_2 - gu_1))
 
 
