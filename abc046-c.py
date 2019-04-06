0bd90ba6644cbb6b52236f185aeecb5a5612d72e
Koichi Ogura <cielo.abierto09@gmail.com>
2019-04-06 23:58:40 +0900

    abc046-c

--- abc046-c.py
+++ abc046-c.py
@@ -7,7 +7,7 @@ for i in range(n):
     li_hi.append(list(map(int, input().split())))
 
 for l in li_hi:
-    w = max(a // l[0], b // l[1])
+    w = max(-(-a // l[0]), -(-b // l[1]))
     a = w * l[0]
     b = w * l[1]
 
