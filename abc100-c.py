e0b0c37312cab0753cdfcd7a461262dd91e900da
Koichi Ogura <12413803+number09@users.noreply.github.com>
2020-07-28 22:01:07 +0900

    abc100-c

--- abc100-c.py
+++ abc100-c.py
@@ -1,24 +1,10 @@
 int_n = int(input())
+li_a = list(map(int, input().split()))
 
-input_array = list(map(int, input().split()))
-
-counter = 0
-
-while True:
-    templist = list(filter(lambda x: x % 2 == 0, input_array))
-    if len(templist) != 0:
-        max_even = max(filter(lambda x: x % 2 == 0, input_array))
-        even_index = input_array.index(max_even)
-
-        input_array = list(map(lambda x: x[1] if x[0] != even_index else x[1] / 2, enumerate(input_array)))
-        print(input_array)
-
-        counter += 1
-
-    else:
-        break
-
-print(counter)
-
-
-
+answer = 0
+for a in li_a:
+    wk = a
+    while wk % 2 == 0:
+        wk = wk // 2
+        answer += 1
+print(answer)
